9c2e4cc3506c5c8b890d0308cb10606e125dae24
myeongseok <myeongseok@kaist.ac.kr>
2025-04-06 15:15:32 -0400

    remove comments

--- lambda_function.py
+++ lambda_function.py
@@ -12,19 +12,16 @@ def lambda_handler(event, context):
     try:
         package_id = event["queryStringParameters"]["packageId"]
 
-        # 현재 시간을 ISO 형식으로 저장
         current_time = datetime.now(UTC).isoformat()
 
-        # DynamoDB에서 패키지 조회
         response = table.get_item(Key={"packageId": package_id})
 
         if "Item" not in response:
             return {"statusCode": 404, "body": "Package not found"}
 
-        valid = True  # 여기서는 임시로 True로 설정했습니다. 실제 비즈니스 로직에 따라 변경 필요
+        valid = True
 
         if valid:
-            # 상태를 'readyForTQ'로 업데이트
             table.update_item(
                 Key={"packageId": package_id},
                 UpdateExpression="SET #status = :status, receiveDate = :receiveDate",
@@ -36,7 +33,6 @@ def lambda_handler(event, context):
             )
             return {"statusCode": 200, "body": "Package status updated to readyForTQ"}
         else:
-            # 상태를 'receiveUnavailable'로 업데이트
             table.update_item(
                 Key={"packageId": package_id},
                 UpdateExpression="SET #status = :status, receiveDate = :receiveDate",
